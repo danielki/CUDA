cf3e600e1c8bf04daa58b46980cbe6d99be681f2
Daniel Kirbst <prayerdaniel@web.de>
2011-04-19 13:58:04 +0200

    2_Aufgabe

---
+++ Blatt_2/main.cu
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+	__global__ void emptyKernel()
+	{
+	// leerer Kernel
+	}
+
+int main(int argc, char ** argv)
+{
+    dim3 dimGrid(1);
+    dim3 dimBlock(1);
+    emptyKernel<<<dimGrid, dimBlock>>>();
+	return 0;
+}
