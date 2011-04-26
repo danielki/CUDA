3b860db92790423f566e60bdbebca6cc842cce15
Daniel Kirbst <prayerdaniel@web.de>
2011-04-26 21:53:30 +0200

    blatt3 init commit

---
+++ Blatt_3/raytracer/src/raytracer.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include "types.h"
+
+// vector operations
+point cross(const point& p1, const point& p2);
+float dot(const point& p1, const point& p2);
+float norm(const point& p);
+void normalize(point& p);
+point operator+(const point& left, const point& right);
+point operator-(const point& left, const point& right);
+point operator*(const float& scalar, const point& p);
+
+// intersection
+bool intersect(const ray& r, const triangle& t, point& intersection);
+
+// initial rays
+void initial_ray(const camera& c, int x, int y, ray& r);
+
+void render_image(const scene& s, const int& height, const int& width, rgb* image);
+
---
+++ Blatt_3/raytracer/src/types.h
@@ -0,0 +1,63 @@
+#pragma once
+
+#include <vector>
+#include <iostream>
+
+// define structs
+struct point {
+    float x;
+    float y;
+    float z;
+};
+
+struct rgb {
+    int r;
+    int g;
+    int b;
+};
+
+struct triangle {
+    point A;
+    point B;
+    point C;
+	rgb farbe;
+};
+
+struct primitives {
+    std::vector <triangle> t; 
+};
+
+struct camera {
+    point position;
+    point richtung;
+	point oben;
+	point obenLinks;
+};
+
+struct ray {
+    point start;
+    point richtung;
+};
+
+struct scene {
+    rgb hintergrund;
+    camera cam;
+    primitives objekte;
+};
+
+// define types
+typedef struct point point;
+typedef struct rgb rgb;
+typedef struct triangle triangle;
+typedef struct primitives primitives;
+typedef struct camera camera;
+typedef struct ray ray;
+typedef struct scene scene;
+
+// define stream output for types
+std::ostream& operator <<(std::ostream& s, const point& p);
+std::ostream& operator <<(std::ostream& s, const rgb& r);
+std::ostream& operator <<(std::ostream& s, const triangle& t);
+std::ostream& operator <<(std::ostream& s, const camera& c);
+std::ostream& operator <<(std::ostream& s, const ray& r);
+
